8d7dfc2672430470bdd22bb7e619e7e5e732da2a
Mengxuan <xiamx2004@gmail.com>
2017-02-08 13:31:12 -0500

    Fix an issue with applying edits

---
+++ test/fixture/schemats/main.ts
@@ -0,0 +1,19 @@
+            /**
+             * AUTO-GENERATED FILE @ 2016-12-07 13:17:46 - DO NOT EDIT!
+             *
+             * This file was generated with schemats node package:
+             * $ schemats generate -c postgres://username:password@localhost/test -t users -o ./test/osm.ts
+             *
+             * Re-run the command above.
+             */
+
+    
+        export namespace cta_situationcompte_scoFields {
+        export type sco_id = number;
+export type sco_total_debit = number;
+export type sco_total_credit = number;
+export type sco_nb_ecritures = number;
+export type pst_id = number;
+export type cpt_id = number;
+
+        }
