8b3156114fc3bb5cbd3f863173d1045bcdbad576
vvakame <vvakame+dev@gmail.com>
2017-02-27 17:12:55 +0900

    feat(tsfmt): add `extends` of tsconfig.json support closes #77

--- lib/provider/tsconfigjson.ts
+++ lib/provider/tsconfigjson.ts
@@ -6,12 +6,6 @@ import * as fs from "fs";
 import { Options } from "../";
 import { getConfigFileName, parseJSON } from "../utils";
 
-interface TsconfigSettings {
-    compilerOptions: {
-        newLine: string;
-    };
-}
-
 export default function makeFormatCodeOptions(fileName: string, opts: Options, formatSettings: ts.FormatCodeSettings): ts.FormatCodeSettings {
 
     let baseDir = opts.baseDir ? path.resolve(opts.baseDir) : path.dirname(path.resolve(fileName));
@@ -23,14 +17,30 @@ export default function makeFormatCodeOptions(fileName: string, opts: Options, f
         console.log(`read ${configFileName} for ${fileName}`);
     }
 
-    let config: TsconfigSettings = parseJSON(fs.readFileSync(configFileName, "utf-8"));
-    if (!config.compilerOptions || !config.compilerOptions.newLine) {
-        return formatSettings;
+    // for `extends` support. It supported from TypeScript 2.1.1.
+    // `& { readFile(path: string): string; }` is backword compat for TypeScript compiler 2.0.3 support.
+    const host: ts.ParseConfigHost & { readFile(path: string): string; } = {
+        useCaseSensitiveFileNames: true,
+        readDirectory: (rootDir, _extensions, excludes, _includes) => {
+            // _extensions -> [ '.ts', '.tsx', '.d.ts' ]
+            // _includes   -> [ '**/*' ]
+
+            const files = fs.readdirSync(rootDir);
+            return files
+                .filter(file => excludes.every(exclude => file !== exclude));
+        },
+        fileExists: path => fs.existsSync(path),
+        readFile: (path: string) => fs.readFileSync(path, "utf-8"),
+    };
+    let rootConfig = parseJSON(fs.readFileSync(configFileName, "utf-8"));
+    let parsed = ts.parseJsonConfigFileContent(rootConfig, host, baseDir);
+    if (parsed.errors && parsed.errors.length !== 0) {
+        throw new Error(parsed.errors.join("\n"));
     }
 
-    if (config.compilerOptions.newLine.toLowerCase() === "crlf") {
+    if (parsed.options.newLine === ts.NewLineKind.CarriageReturnLineFeed) {
         formatSettings.newLineCharacter = "\r\n";
-    } else if (config.compilerOptions.newLine.toLowerCase() === "lf") {
+    } else if (parsed.options.newLine === ts.NewLineKind.LineFeed) {
         formatSettings.newLineCharacter = "\n";
     }
 
