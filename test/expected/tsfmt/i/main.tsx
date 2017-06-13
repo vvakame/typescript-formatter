d33dd0f6e18907274fd165648ba9532015cd4905
vvakame <vvakame+dev@gmail.com>
2017-06-13 18:13:29 +0900

    chore(tsfmt): add test for #102

---
+++ test/expected/tsfmt/i/main.tsx
@@ -0,0 +1,8 @@
+<View style={styles.container}>
+    <Text>
+        I am a lightbox!
+        </Text>
+    <TouchableOpacity onPress={this.dismissLightBox.bind(this)}>
+        <Text>Close Me!</Text>
+    </TouchableOpacity>
+</View>
