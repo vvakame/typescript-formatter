60c2919468eccc5e794f1a0052aad704a9b6e573
vvakame <vvakame+dev@gmail.com>
2017-09-08 11:10:07 +0900

    refactor(tsfmt): refactor own LanguageServiceHost implementation

--- lib/formatter.ts
+++ lib/formatter.ts
@@ -3,7 +3,7 @@ import * as ts from "typescript";
 import { createDefaultFormatCodeSettings } from "./utils";
 
 class LanguageServiceHost implements ts.LanguageServiceHost {
-    files: { [fileName: string]: ts.IScriptSnapshot; } = {};
+    files: ts.MapLike<ts.IScriptSnapshot> = {};
     addFile(fileName: string, text: string) {
         this.files[fileName] = ts.ScriptSnapshot.fromString(text);
     }
@@ -12,10 +12,10 @@ class LanguageServiceHost implements ts.LanguageServiceHost {
 
     getCompilationSettings = () => ts.getDefaultCompilerOptions();
     getScriptFileNames = () => Object.keys(this.files);
-    getScriptVersion = (_fileName: string) => "1";
+    getScriptVersion = (_fileName: string) => "0";
     getScriptSnapshot = (fileName: string) => this.files[fileName];
     getCurrentDirectory = () => process.cwd();
-    getDefaultLibFileName = (_options: ts.CompilerOptions) => "lib";
+    getDefaultLibFileName = (options: ts.CompilerOptions) => ts.getDefaultLibFilePath(options);
 }
 
 export function format(fileName: string, text: string, options = createDefaultFormatCodeSettings()) {
